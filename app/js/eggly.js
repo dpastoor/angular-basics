87c778bd9a761f39fcaefe551a5c89007f3be3df
Devin Pastoor <devin.pastoor@gmail.com>
2016-01-02 22:29:34 -0800

    add some notes

--- app/js/eggly.js
+++ app/js/eggly.js
@@ -1,7 +1,8 @@
 angular.module('Eggly', [
-  //arry of dependencies
+  //array of dependencies
 ])
 .controller('MainCtrl', function($scope) {
+  // scope is the glue between the controller and the View
   $scope.title = "Welcome!";
   $scope.messages = {
     1: {title: 'title1', text: 'message1'},
