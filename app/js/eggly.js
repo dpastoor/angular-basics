1e05591d844090a0e8150aacb34a93f6f17040d0
Devin Pastoor <devin.pastoor@gmail.com>
2016-01-02 22:28:23 -0800

    hook up basic ngrepeat

--- app/js/eggly.js
+++ app/js/eggly.js
@@ -3,4 +3,8 @@ angular.module('Eggly', [
 ])
 .controller('MainCtrl', function($scope) {
   $scope.title = "Welcome!";
+  $scope.messages = {
+    1: {title: 'title1', text: 'message1'},
+    2: {title: 'title2', text: 'message2'}
+  };
 });
