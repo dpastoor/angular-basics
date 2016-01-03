7fe71c533e358f6532aeec7fbb900fc53118d589
Devin Pastoor <devin.pastoor@gmail.com>
2016-01-03 10:23:33 -0800

    add categories

--- app/js/eggly.js
+++ app/js/eggly.js
@@ -4,8 +4,12 @@ angular.module('Eggly', [
 .controller('MainCtrl', function($scope) {
   // scope is the glue between the controller and the View
   $scope.title = "Welcome!";
-  $scope.messages = {
-    1: {title: 'title1', text: 'message1'},
-    2: {title: 'title2', text: 'message2'}
-  };
+    $scope.messages = {
+      1: {title: 'title1', text: 'message1', category: 'cat1'},
+      2: {title: 'title2', text: 'message2', category: 'cat1'},
+      3: {title: 'title3', text: 'message3', category: 'cat2'},
+      4: {title: 'title4', text: 'message4', category: 'cat2'},
+      5: {title: 'title5', text: 'message5', category: 'cat3'},
+      6: {title: 'title6', text: 'message6', category: 'cat3'}
+    };
 });
